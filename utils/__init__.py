9e00de3c717e79d8e85474f3018d662665b7b2f7
Tala Hethnawi <hethnawi.tala@gmail.com>
2021-11-11 13:56:14 +0200

    Turn off SQLALCHEMY echo

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///./db.sqlite3', echo=True, connect_args={"check_same_thread": False})
+engine = create_engine('sqlite:///./db.sqlite3', echo=False, connect_args={"check_same_thread": False})
 Session = sessionmaker(bind=engine)
 session = Session()
